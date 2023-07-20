a4c3c9815a4c6582296854a0ad5a305b78c391fb
Pratham Nagaria <prathamnagaria@gmail.com>
2023-07-20 12:32:43 +0530

    First Commit - Established Server & Mongo DB connection

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+const express= require('express')
+const cors = require('cors')
+const db =require('./database/db')
+// const {readdirSync} = require('fs')
+
+
+const app = express();
+
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+
+// middlewares
+
+app.use(express.json())  // for parsing incoming requests with JSON data
+app.use(cors())
+
+
+//routes
+
+// readdirSync('./routes').map((route) => app.use('/api/v1',require('./routes/' + route)))
+
+
+
+
+
+const server = () => {
+    db();
+    app.listen(PORT, () => {
+        console.log('listening to port', PORT);
+    })
+}
+
+server();
---
+++ backend/database/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const db = async () => {
+    try {
+        mongoose.set('strictQuery',false);
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log('Mongo DB successfully connected')
+    } catch (error) {
+        console.log('Mongo DB Connection error',error);
+    }
+}
+
+module.exports = db
